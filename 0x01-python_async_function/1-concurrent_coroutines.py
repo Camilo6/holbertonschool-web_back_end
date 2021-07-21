f4077ac3f9823a382b0be8ca66c478a4bc531ef2
Camilo <trresqwe@gmail.com>
2021-07-21 15:49:45 -0500

    Update

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """a asynchronous coroutine"""
+import asyncio
+from typing import List
 
-import asyncio  
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n : int, max_delay: int) -> float:
-    """returns a delayed value"""
-    delayed_value = wait_random(n)
-    await asyncio.sleep(delayed_value)
-    return delayed_value
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """returns list of all delayed async tasks"""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delayed_tasks = [await task for task in asyncio.as_completed(tasks)]
+    return delayed_tasks
