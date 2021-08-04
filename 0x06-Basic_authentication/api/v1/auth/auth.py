4d3494dbfaa8695f1e9b6f51c03aea78f1456f85
Camilo <trresqwe@gmail.com>
2021-08-04 16:28:17 -0500

    Task 4

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,26 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''self descriptive'''
-        return False
+        if not path or not excluded_paths:
+            return True
+
+        path += '/' if path[-1] != '/' else ''
+        wildcard = any(rex.endswith("*") for rex in excluded_paths)
+
+        if not wildcard:
+            if path in excluded_paths:
+                return False
+
+        for rex in excluded_paths:
+            if rex[-1] == '*':
+                if path.startswith(rex[:-1]):
+                    return False
+            if rex == path:
+                return False
+        return True
+
+
+
 
     def authorization_header(self, request=None) -> str:
         '''self descriptive'''
