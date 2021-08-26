463d5c88c3f271421abde42f9b5971361e298eb2
Camilo <trresqwe@gmail.com>
2021-08-26 15:46:19 -0500

    Update

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-''' Basic Flas app with Babel'''
+""" Basic Flask app
+"""
 from flask import Flask, render_template
-from flask_baberl import Babel
-
+from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config():
-    '''babel config class'''
+class Config(object):
+    ''' babel config class '''
     LANGUAGES = ["en", "fr"]
 
 
@@ -16,17 +16,17 @@ app.config.from_object(Config)
 Babel.default_locale = 'en'
 Babel.default_timezone = 'UTC'
 
-
 @babel.localeselector
 def get_locale():
     '''get locale funciont'''
     return request.acceptlenguajes.best_match(Config.LANGUAGES)
 
 
-@app.route('/', methods=["GET"])
+@app.route("/", methods=["GET"])
 def index():
-    return (render_template('2-index.html'))
+    """ Returns index """
+    return render_template("0-index.html")
 
 
-if (__name__ == '__main__'):
-    app.run(port=3000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
