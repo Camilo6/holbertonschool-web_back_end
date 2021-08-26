c9f06a445658f61163de17ab972cd4139e15859f
Camilo <trresqwe@gmail.com>
2021-08-26 16:03:17 -0500

    Update

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -25,7 +25,7 @@ def get_locale():
 @app.route("/", methods=["GET"])
 def index():
     """ Returns index """
-    return render_template("0-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
