4267cd92841e97e8e6ba41100014683410f0a940
Camilo <trresqwe@gmail.com>
2021-08-26 16:20:24 -0500

    Update

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
-"""
-Flask App.
-"""
-from flask import Flask, render_template, request
-from os import getenv
-from flask_babel import Babel, gettext
+""" Module for trying out Babel i18n """
+from flask_babel import Babel, _
+from flask import Flask, render_template, request, flash
 
-
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
 
 
 class Config(object):
-    """ Config Class."""
+    """ Configuration Class for Babel """
+
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -21,19 +18,17 @@ class Config(object):
 app.config.from_object(Config)
 
 
-@app.route('/')
-def index():
-    """Render index.html."""
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world() -> str:
+    """Renders a Basic Template for Babel Implementation"""
     return render_template("3-index.html")
 
 
 @babel.localeselector
-def get_locale():
-    """Determine the bestmatch languages."""
+def get_locale() -> str:
+    """Select a language translation to use for that request"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run()
