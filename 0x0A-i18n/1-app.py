fa798185df089f596731df0dbd15221d2e44df99
Camilo <trresqwe@gmail.com>
2021-08-26 15:08:57 -0500

    Update

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-''' Basic Flas app with Babel'''
+""" Basic Flask app
+"""
 from flask import Flask, render_template
-from flask_baberl import Babel
-
+from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config():
-    '''babel config class'''
+class Config(object):
+    ''' babel config class '''
     LANGUAGES = ["en", "fr"]
 
 
@@ -17,10 +17,11 @@ Babel.default_locale = 'en'
 Babel.default_timezone = 'UTC'
 
 
-@app.route('/', methods=["GET"])
+@app.route("/", methods=["GET"])
 def index():
-    return (render_template('1-index.html'))
+    """ Returns index """
+    return render_template("0-index.html")
 
 
-if (__name__ == '__main__'):
-    app.run(port=3000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
