5907361a9e8484980f885e96e99a9fc337a9d247
Camilo <trresqwe@gmail.com>
2021-08-25 22:28:50 -0500

    Update 0-1

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+''' Basic Flas app with Babel'''
+from flask import Flask, render_template
+from flask_baberl import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    '''babel config class'''
+    LANGUAGES = ["en", "fr"]
+
+
+
+app.config.from_object(Config)
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
+
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=["GET"])
+def index():
+    return (render_template('0-index.html'))
+
+
+if (__name__ == '__main__'):
+    app.run(port=3000, debug=True)
