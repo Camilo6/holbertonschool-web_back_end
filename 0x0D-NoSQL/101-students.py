e85249519b0b060b1c281e513f23ed87bfa8a5b9
Camilo <trresqwe@gmail.com>
2021-10-04 23:17:11 -0500

    Update

---
+++ 0x0D-NoSQL/101-students.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" 101-students """
+import pymongo
+
+
+def top_students(mongo_collection):
+    """ Top Students """
+    studentlist = list(mongo_collection.find())
+    students = []
+    for student in studentlist:
+        mystudent = {"name": student.get("name"), "_id": student.get("_id")}
+        total = 0
+        for myscore in student.get("topics"):
+            total += myscore.get("score")
+        mystudent["averageScore"] = total / len(student.get("topics"))
+        students.append(mystudent)
+    return sorted(students, key=lambda i: i["averageScore"], reverse=True)
